10e447b40f9281584fb63bcac567fc854e60fc9a
mgwebb <mark.webb@cgi.com>
2017-06-07 10:27:07 -0600

    added missing import

--- src/main/java/org/cloudfoundry/Application.java
+++ src/main/java/org/cloudfoundry/Application.java
@@ -2,6 +2,7 @@ package org.cloudfoundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
